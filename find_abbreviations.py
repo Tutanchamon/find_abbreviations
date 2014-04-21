774fcdaac8d9080a3f1a6478eb0645cb8ee884f6
Tutanchamon <tutenchamon@gmail.com>
2014-04-21 16:55:41 +0200

    Separate list for abbreviations found and not found in given file, coverage and precision added

--- find_abbreviations.py
+++ find_abbreviations.py
@@ -16,7 +16,6 @@
 
 import sys
 import re
-import operator
 import getopt
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -30,12 +29,21 @@ VOWELS = [ch for ch in "aąeęioóuy"]
 digits = re.compile("^[0-9]+$")
 
 ### Polish abbreviations patterns
-single_letter_pattern = re.compile("^[A-Za-z]\.$")   # single letter, for example: "a." (stands for "albo") or "B." like in "President B. Obama"
-vowels = re.compile("^([a-ząćęłńóśźż]\.)+$")     # abbreviations with vowels, example: p.n.e. (przed naszą erą),
-only_consonants = re.compile("^[bcćdfghjklłmnńpqrsśtvwxzźż]+\.$")   # only consonants (example: np. (na przykład), jw. (jak wyżej))
-followed_by_punc = re.compile("[a-ząćęłńóśźż]+\.[,;:]$")                         # followed by punctuation, ","
-with_hyphen = re.compile("[a-ząćęłńóśźż]+\.-[a-ząćęłńóśźż]+\.$")    # two words connected with a hyphen, (e.g. "pd.-zach.")
-more_than_one = re.compile("([a-ząćęłńóśźż]+\.){2,}$")      # many connected abbreviations (e.g. 'dz.urz.')
+
+# single letter, for example: "a." (stands for "albo") or "B." like in "President B. Obama"
+single_letter_pattern = re.compile("^[A-Za-z]\.$")
+# abbreviations with vowels, example: p.n.e. (przed naszą erą),   
+vowels = re.compile("^([a-ząćęłńóśźż]\.)+$")
+# only consonants (example: np. (na przykład), jw. (jak wyżej))     
+only_consonants = re.compile("^[bcćdfghjklłmnńpqrsśtvwxzźż]+\.$")
+# followed by punctuation, ","   
+followed_by_punc = re.compile("[a-ząćęłńóśźż]+\.[,;:]$")
+# two words connected with a hyphen, (e.g. "pd.-zach.")
+with_hyphen = re.compile("[a-ząćęłńóśźż]+\.-[a-ząćęłńóśźż]+\.$")
+# many connected abbreviations (e.g. 'dz.urz.')    
+more_than_one = re.compile("([a-ząćęłńóśźż]+\.){2,}$")
+# if the first word ends with a dot and the second one begins with a small letter      
+bigram_rule = re.compile("^[a-z]+\. [a-ząćężźółńś]+$")   
 
 abbr_patterns = [
     single_letter_pattern,
@@ -48,7 +56,24 @@ abbr_patterns = [
 
 abbr_dictionary = {}    # dictionary of abbreviation and count of occurences
 pattern_matches = {}    # dictionary of patterns and how many abbreviations they found
+found_in_list = []
+not_found_in_list= {}
 
+""" 
+Finds abbreviations in bigrams. If the first word ends with
+a dot and the second one starts with a small letter,
+the first one is treated as an abbreviation
+"""
+def bigram_find(sequence, number=3):
+    for i in range(len(sequence)-number+1):
+        #sys.stdout.write("%s\n" % sequence[i:i+number])
+        word_to_check = sequence[i] + ' ' + sequence[i+1]
+        if bigram_rule.match(word_to_check):
+            abbr_dictionary[sequence[i]] = abbr_dictionary.get(sequence[i], 0) + 1
+            pattern_matches[bigram_rule] = pattern_matches.get(bigram_rule, 0) + 1
+"""
+Creates a list of abbreviations from given file
+"""            
 def get_abbreviations_from_file(location):
     print 'The file with list of abbreviations was named: ' + location[0]
     abbr_file = open(location[0], 'r')
@@ -57,20 +82,35 @@ def get_abbreviations_from_file(location):
         abbr_list.append(line.strip(' \t\n\r'))
     return abbr_list
 
+"""
+Writes short statistics about abbreviations found in given file
+"""
 def write_abbreviations_from_file_stats(abbr_file_list, abbr_dictionary):
+    for abbr in sorted(abbr_dictionary):
+        if abbr in abbr_file_list:
+            found_in_list.append(abbr)
+        else:
+            not_found_in_list[abbr] = abbr_dictionary[abbr]
+    tp = len(found_in_list) # found in list
+    fp = len(not_found_in_list)
+    if tp > 0:
+        precision = float(tp) / float(tp + fp)
+        coverage = float(tp) / float(len(abbr_file_list))
+    print '\n'
     print '==============================='
     print 'Number of abbreviations in abbreviations list file: ' + str(len(abbr_file_list))
     print 'Number of abbreviations found by script: ' + str(len(abbr_dictionary))
+    print 'Precision = ', str(tp), '/ (', str(tp), ' + ', str(fp), ') = ', str(precision)
+    print 'Coverage = ', str(tp), ' / ', str(len(abbr_file_list)), ' = ', str(coverage)
     print '==============================='
     
-
 def main(argv):
     abbr_list_file_location = ''
     is_summary = False
     abbr_file_list = []
     ### Parsing options and arguments
     try:
-      opts, args = getopt.getopt(argv,"hfs",["abbr_file="])
+      opts, args = getopt.getopt(argv, "hfs", ["abbr_file="])
     except getopt.GetoptError:
         print 'Usage: find_abbreviations.py [-f file_with_abbreviations.txt]'
         sys.exit(2)
@@ -97,6 +137,9 @@ def main(argv):
                             token = token[:-1]
                         abbr_dictionary[token] = abbr_dictionary.get(token, 0) + 1
                         pattern_matches[pattern] = pattern_matches.get(pattern, 0) + 1
+        # testing bigram_rule
+        bigram_find(tokens)
+        
     #for abbr in sorted(abbr_dictionary):
     #    print(abbr + "\t " + str(abbr_dictionary[abbr]))
     
@@ -114,7 +157,7 @@ def main(argv):
     
     # Simple print of results - abbreviations       number of occurences
     for sorted_item in sorted(abbr_dictionary, key=abbr_dictionary.get, reverse=True):
-      print sorted_item, "\t\t", abbr_dictionary[sorted_item]
+        print sorted_item, "\t\t", abbr_dictionary[sorted_item]
       
     if abbr_list_file_location:
         abbr_file_list = get_abbreviations_from_file(abbr_list_file_location)
@@ -122,6 +165,7 @@ def main(argv):
             write_abbreviations_from_file_stats(abbr_file_list, abbr_dictionary)
         
     if is_summary:
+        print '\n'
         print '==============================='
         print 'How many abbreviations were found by each pattern:'
         print '==============================='
@@ -129,21 +173,26 @@ def main(argv):
             print(patt.pattern + "\t " + str(pattern_matches[patt]))
             
     if is_summary & (len(abbr_file_list) > 0):
-        print '==============================='
-        print 'Which abbreviations were wound on the list'
-        print '==============================='
-        for abbr in sorted(abbr_dictionary):
-            print abbr,
-            if abbr in abbr_file_list:
-                print '\t\tfound on the list'
-            else:
-                print '\t\tnot found on the list'
+        
         #print 'elements from file'
         #for element in abbr_file_list:
             #print element
+        print '\n'
+        print '==============================='
+        print 'Abbreviations which were found on the list'
+        print '==============================='
+        for abbr in found_in_list:
+            print abbr
+        print '\n'
+        print '==============================='
+        print 'Abbreviations which were not found on the list'
+        print '==============================='
+        for sorted_item in sorted(not_found_in_list, key=not_found_in_list.get, reverse=True):
+            print sorted_item, "\t\t", not_found_in_list[sorted_item]
+        
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
         
         
         
